7a06fc7cc60304d129f1d38d0c7f934504226f06
Solomonitie <solomonitie@gmail.com>
2022-09-09 09:08:30 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the input function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return 0
-*/
+ * main - printing
+ *
+ * Return: Always 0 (Successful)
+ */
 int main(void)
 {
-       puts("/"programming is like building a multilingual puzzle");
-       return (0);
+     puts("\"Programming is like building a multilingual puzzle");
+     
+     return (0);
 }
