12901af1b278dd43070efadcdf86da826c6e6b2e
Solomonitie <solomonitie@gmail.com>
2022-09-09 10:02:34 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,14 @@
-#include <stdio.h>                                                                                                                                 
+#include <stdio.h>
 
-                                                                                                                                                   
-
-/**                                                                                                                                                
-
-* main - printing                                                                                                                                  
-
-*                                                                                                                                                  
-
-* Return: Always 0 (Successful)                                                                                                                    
-
-*/                                                                                                                                                 
-
-int main(void)                                                                                                                                     
-
-{                      
-printf("with proper grammar, but the outcome is a piece of art,\n");
+/**
+* main - prints a text
+*
+* Return: Always 0 (Successful)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is piece of art,\n");
+return (0)
 
 return (0);
 }
