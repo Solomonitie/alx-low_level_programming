8e20cd7168a052ca9f3479d35234dab8bf803ddd
Solomonitie <solomonitie@gmail.com>
2022-09-09 15:22:24 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
-
-#include <stdio.hh>
+#include <stdio.h>
 
 /**
 *main - prints a text
@@ -8,6 +7,7 @@
 */
 int main(void)
 {
+
 printf("with proper grammar, but the outcome is a piece of art,\n");
 
 return (0);
