096018f59045aa42ad0f0ded5eaac25b1c3f54c8
Solomonitie <solomonitie@gmail.com>
2022-09-15 19:03:41 -0700

    3-print_numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,18 +5,11 @@
 
 
 /**
-
-*print_numbers - function that print all numbers from
-
-*0 to 9
-
-*
-
-*Return: returns nothing
-
-*/
-
-
+ * print_numbers - function that print all numbers from
+ * 0 to 9
+ *
+ * Return: returns nothing
+ */
 
 void print_numbers(void)
 
